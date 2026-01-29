ede2e2144578f0b11e6c3b81066f0981b07ec0ff
Parry-Zhuo <165851545+Parry-Zhuo@users.noreply.github.com>
2026-01-28 22:21:02 -0800

    both boards programmed with blinky

---
+++ src/cube/blinky/main.py
@@ -0,0 +1,17 @@
+#python -m mpremote cp main.py :
+#python -m mpremote connect COM7 repl
+#python -m mpremote reset
+
+#then press reset or 
+import time
+from machine import Pin
+
+
+print("main.py running")
+
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.toggle()
+    time.sleep(1)
+

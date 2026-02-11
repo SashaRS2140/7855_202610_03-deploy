e94110f400e6b39148cea3db89fa27813d8e38be
SashaRS2140 <sasharoosen2140@gmail.com>
2026-02-11 15:28:06 -0800

    Refactor: Switch database from TinyDB to Firestore

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,18 +1,22 @@
 import os
 from flask import Flask
-from .data.repository import DatabaseRepository
+from .data.repository import Repository
 from .services.session_services import SessionService
 from .services.cube_services import CubeInterfaceService
 from .presentation.web_controller import web_bp
 from .presentation.api_controller import api_bp
+
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = os.getenv("SECRET_KEY", "dev-secret")
 
     # 1. Initialize Data Layer
-    db_path = os.path.join(os.path.dirname(__file__), 'data', 'db.json')
-    repository = DatabaseRepository(db_path)
+    # OLD: db_path = os.path.join(os.path.dirname(__file__), 'data', 'db.json') <-- DELETE THIS
+
+    # IMPORTANT: Ensure the class name here ('Repository' )
+    # matches exactly what you defined in repository.py
+    repository = Repository()
 
     # 2. Initialize Service Layer (Injecting Repository)
     app.session_service = SessionService(repository)

74b142768443939f1df958b9e933ec084f2e9ac4
SashaRS2140 <sasharoosen2140@gmail.com>
2026-02-16 13:56:36 -0800

    Updated Web Gui Nearly Working Login - & Broken Main

--- src/server/data/repository.py
+++ src/server/data/repository.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase only once
+
 if not firebase_admin._apps:
     cred = credentials.Certificate("src/server/serviceAccountKey.json")
     firebase_admin.initialize_app(cred)
--- src/server/presentation/web_controller.py
+++ src/server/presentation/web_controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, current_app
 import os
 
-static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../static')
 
 web_bp = Blueprint('web', __name__,
                    template_folder='templates',
